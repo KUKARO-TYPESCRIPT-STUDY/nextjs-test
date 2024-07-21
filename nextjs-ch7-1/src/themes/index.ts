212c64cfdf38d1b5d3e4715a91738e3f750d1034
justk <justkukaro@naver.com>
2024-07-21 15:40:50 +0900

    feat:스토리 변경전 체크 포인트

---
+++ nextjs-ch7-1/src/themes/index.ts
@@ -0,0 +1,13 @@
+import colors from '@/themes/colors'
+import space from '@/themes/space'
+import fontSizes from '@/themes/fontSizes'
+import letterSpacings from '@/themes/letterSpacings'
+import lineHeights from '@/themes/lineHeights'
+
+export const theme = {
+  space,
+  colors,
+  fontSizes,
+  letterSpacings,
+  lineHeights,
+} as const
